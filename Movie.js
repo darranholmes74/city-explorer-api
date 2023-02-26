b92eb0f9308ac1855106aee724aac59db5031708
Darran Holmes <darranholmes74@gmail.com>
2023-02-26 13:01:41 -0800

    turning in lab 10

--- Movie.js
+++ Movie.js
@@ -1,6 +1,7 @@
 let axios = require('axios')
 const MOVIE_TOKEN = process.env.MOVIE_API_KEY
 
+let cache = require("./cache")
 
 class Movie {
     constructor(movieData){
@@ -9,20 +10,54 @@ class Movie {
     }
 }
 
-let getMovie = async (request, response) => {
-    let cityName = request.query.cityName
-    let proxy = {
-        url: `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_TOKEN}&language=en-US&page=1&include_adult=false&query=${cityName}`,
-        method: 'get'
-    };
 
-    let movieProxy = await axios(proxy);
-    console.log(movieProxy)
-    let movieArray = [];
-    movieProxy.data.results.forEach(movies => {
-        movieArray.push(new Movie(movies));
-    });
-        response.status(200).send(movieArray)
+
+
+
+
+function getMovie(query){
+    const cacheKey = 'movie-' + query;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_TOKEN}&language=en-US&page=1&include_adult=false&query=${cityName}`;
+
+    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < 30000)){
+        console.log('cache hits');
+    } else {
+        console.log("cache miss");
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+        cache[key].data = axios.get(url)
+        .then(response => parseM(response.data));
     }
+}
+
+function parseM(movieData){
+    try {
+        let movieResults = [];
+        movieData.movieResults.map(movies => {
+            movieResults.push(new Movie(movies));
+        });
+        return Promise.resolve(movieResults);
+    } catch (e){
+        return Promise.reject(e)
+    }
+}
+
+
+
+// let getMovie = async (request, response) => {
+//     let cityName = request.query.cityName
+//     let proxy = {
+//         url: `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_TOKEN}&language=en-US&page=1&include_adult=false&query=${cityName}`,
+//         method: 'get'
+//     };
+
+//     let movieProxy = await axios(proxy);
+//     console.log(movieProxy)
+//     let movieArray = [];
+//     movieProxy.data.results.forEach(movies => {
+//         movieArray.push(new Movie(movies));
+//     });
+//         response.status(200).send(movieArray)
+//     }
 
     module.exports = getMovie;
--- Weather.js
+++ Weather.js
@@ -1,24 +1,57 @@
 let axios = require('axios')
+let cache = require('./cache')
 
 const WEATHER_TOKEN = process.env.WEATHER_API_KEY
 console.log(WEATHER_TOKEN);
-let getWeather = async (request, response) => {
-    let city_name = request.query.city_name
-    let proxy = {
-        url: `https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${WEATHER_TOKEN}`,
-        method: 'GET',
-    };
 
-    let weatherProxy = await axios(proxy);
-    console.log(weatherProxy.data)
-    let weatherArray = [];
-    weatherProxy.data.data.forEach(weather => {
-        weatherArray.push(new Forecast(weather));
-    });
+
+
+function getWeather(query){
+    const cacheKey = 'weather-' + query;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${WEATHER_TOKEN}`;
     
-        response.status(200).send(weatherArray)
-
+    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < 30000)){
+        console.log('cache hits');
+    } else {
+        console.log("cache miss");
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+        cache[key].data = axios.get(url)
+        .then(response => parseM(response.data));
+    }
 }
+
+function parseW(weatherData){
+    try {
+        let weatherResults = [];
+        weatherData.weatherResults.map(weather => {
+            weatherResults.push(new Movie(weather));
+        });
+        return Promise.resolve(weatherResults);
+    } catch (e){
+        return Promise.reject(e)
+    }
+}
+
+
+
+// let getWeather = async (request, response) => {
+//     let city_name = request.query.city_name
+//     let proxy = {
+//         url: `https://api.weatherbit.io/v2.0/forecast/daily?city=${city_name}&key=${WEATHER_TOKEN}`,
+//         method: 'GET',
+//     };
+
+//     let weatherProxy = await axios(proxy);
+//     console.log(weatherProxy.data)
+//     let weatherArray = [];
+//     weatherProxy.data.data.forEach(weather => {
+//         weatherArray.push(new Forecast(weather));
+//     });
+    
+//         response.status(200).send(weatherArray)
+
+// }
 
 class Forecast {
     constructor(weatherData){
